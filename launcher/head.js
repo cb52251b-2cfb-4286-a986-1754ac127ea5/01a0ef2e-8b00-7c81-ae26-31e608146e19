93b1f839c06b959b71d3c911bcb73615543d2c16
cb52251b-2cfb-4286-a986-1754ac127ea5 <letidoyle@gmail.com>
2026-08-13 14:12:21 +0200

    turn off bookmarklet

--- launcher/head.js
+++ launcher/head.js
@@ -1,4 +1,4 @@
-javascript:(async function () {
+/*javascript:*/(async function () {
   const baseUrl = "https://cb52251b-2cfb-4286-a986-1754ac127ea5.github.io/01a0ef2e-8b00-7c81-ae26-31e608146e19/";
   const files = ["headings.js", "links.js"];
   const results = [];
