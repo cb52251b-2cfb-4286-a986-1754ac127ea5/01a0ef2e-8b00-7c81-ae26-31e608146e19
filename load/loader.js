ffb1c5343f692b43f9c0b63fe5a773545d51721d
cb52251b-2cfb-4286-a986-1754ac127ea5 <letidoyle@gmail.com>
2026-09-07 12:14:24 +0200

    add ability to snipe remove prior scripts

--- load/loader.js
+++ load/loader.js
@@ -745,6 +745,7 @@
         document.body.classList.remove(TEST_CLASS); //2148
         const style2148 = document.getElementById(STYLE_ID); //2148
         if (style2148) style2148.remove(); //2148
+        document.querySelectorAll('[data-scope="__bar318a5bb8aaf3e"]').forEach((el)=>{el.remove()}); //snipe remove prior execution files
 
         function cssVar(s) {
             return getComputedStyle(document.documentElement).getPropertyValue(s);
